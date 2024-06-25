b25a6ea824b98301427e63e1a49b91b40a54d0bf
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2024-06-25 12:25:07 +0300

    if on mobile view dont use ballcanvas added active

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,50 +1,73 @@
-import React, { Suspense } from 'react';
+import React, { useEffect, useState, Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
 const Ball = (props) => {
-  const [decal] = useTexture([ props.imgUrl ]);
+  const [decal] = useTexture([props.imgUrl]);
   return (
-    <Float speed={2.5} rotationIntensity={1} floatIntensity={2} >
-      <ambientLight intensity={0.25}/>
-      <directionalLight position={[0, 0, 0.05]}/>
+    <Float speed={2.5} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial 
+        <meshStandardMaterial
           color='#fff8eb'
           polygonOffset
           polygonOffsetFactor={-7}
           flatShading
         />
-        <Decal 
+        <Decal
           position={[0, 0, 1]}
-          rotation={[ 2 * Math.PI, 0, 6.25]}
+          rotation={[2 * Math.PI, 0, 6.25]}
           flatShading
           map={decal}
         />
       </mesh>
     </Float>
-  )
-}
+  );
+};
 
 const BallCanvas = ({ icon }) => {
-  return(
-    <Canvas 
-      frameLoop="demand"
-      gl={{ preserveDrawingBuffer: true }}
-      >
-        <Suspense fallback={<CanvasLoader />}>
-          <OrbitControls enableZoom={false} />
-          <Ball imgUrl={icon}/>
+  const [isMobile, setIsMobile] = useState(false);
 
-        </Suspense>
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
 
-        <Preload all/>
-    </Canvas>
-  )
+    setIsMobile(mediaQuery.matches);
 
-}
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
 
-export default BallCanvas
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <>
+      {isMobile ? (
+        <div className="icon-container">
+          <img src={icon} alt="icon" className="icon" />
+        </div>
+      ) : (
+        <Canvas
+          frameLoop="demand"
+          gl={{ preserveDrawingBuffer: true }}
+        >
+          <Suspense fallback={<CanvasLoader />}>
+            <OrbitControls enableZoom={false} />
+            <Ball imgUrl={icon} />
+          </Suspense>
+          <Preload all />
+        </Canvas>
+      )}
+    </>
+  );
+};
+
+export default BallCanvas;
